064653fbb66add6c5cf3bdcb584e2c23a1df930a
medch1 <chachia.mohamed@gmail.com>
2021-07-20 13:16:55 +0100

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -23,6 +23,35 @@ export class ContactComponent implements OnInit {
 
   @ViewChild('fform') feedbackFormDirective:any;
 
+  formErrors:any={
+    'firstname':'',
+    'lastname':'',
+    'telnum':'',
+    'email':''
+  };
+
+  validationMessages:any ={
+    'firstname':{
+      'required':'First name is required.',
+      'minlength':'First name must be at least 2 characters long',
+      'maxlength':'First name cannot be more than 25 characters'
+    },
+    'lastname':{
+      'required':'Last name is required.',
+      'minlength':'Last name must be at least 2 characters long',
+      'maxlength':'Last name cannot be more than 25 characters'
+    },
+    'telnum': {
+      'required':'Tel. number is required.',
+      'pattern':'Tel. number must contain only numbers.'
+    },
+    'email':{
+      'required':'Email is required.',
+      'email':'Email not in valid format.'
+
+    }
+
+  };
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -34,15 +63,41 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required ],
-      lastname: ['', Validators.required ],
-      telnum: ['', Validators.required ],
-      email: ['', Validators.required ],
+      firstname: ['', [Validators.required,Validators.minLength(2),Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required,Validators.minLength(2),Validators.maxLength(25)] ],
+      telnum: [0, [Validators.required,Validators.pattern ]],
+      email: ['', [Validators.required,Validators.email] ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+    this.feedbackForm.valueChanges
+      .subscribe(data =>this.onValueChanged(data));
+
+    this.onValueChanged();
   }
+
+  onValueChanged(data?:any){
+    if(!this.feedbackForm) {
+      return;
+    }
+    const form=this.feedbackForm;
+    for (const field in this.formErrors) {
+      if(this.formErrors.hasOwnProperty(field)){
+        this.formErrors[field]='';
+        const control = form.get(field);
+        if(control && control.dirty && !control.valid ) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors){
+            if(control.errors.hasOwnProperty(key)){
+              this.formErrors[field] += messages[key] + '';
+            }
+          }
+        }
+      }
+    }
+  }
+
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,6 +3,7 @@ import { Dish } from '../shared/dish';
 import {Params,ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
 import {DishService} from "../services/dish.service";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,15 +13,29 @@ import {DishService} from "../services/dish.service";
 export class DishdetailComponent implements OnInit {
 
   dish: Dish | undefined;
+  dishIds: string[] | undefined;
+  prev: string | undefined;
+  next: string | undefined;
   constructor(private route:ActivatedRoute,
               private dishService:DishService,
               private  location:Location) { }
 
-  ngOnInit(): void {
-    const id=this.route.snapshot.params['id'];
-    this.dishService.getdish(id)
-      .subscribe(dish=>this.dish=dish);
+
+  ngOnInit() {
+    this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params.pipe(switchMap((params: Params) => this.dishService.getdish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
+  setPrevNext(dishId:string|undefined) {
+    if (dishId != null) {
+      const index = this.dishIds?.indexOf(dishId);
+
+      this.prev = this.dishIds?.[(this.dishIds?.length + index! - 1) % this.dishIds?.length];
+      this.next = this.dishIds?.[(this.dishIds?.length + index! + 1) % this.dishIds?.length];
+    }
+  }
+
+
   goBack():void{
 this.location.back();
   }
